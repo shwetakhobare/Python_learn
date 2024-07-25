49880da929e3f1797b0b9346b8862f5d3ce92c28
shwetakhobare <shwetakhobare123@gmail.com>
2024-07-25 12:53:28 +0530

    The python6.py file is added

---
+++ python6.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second  number :"))
+c = int(input("Enter the third number :"))
+
+s = (a+b+c)/2
+
+Area = (s*(s-a)*(s-b)*(s-c))**0.5
+print("The area of triangle is",Area)

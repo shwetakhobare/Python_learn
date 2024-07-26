b6273a3efcddc28ba9a17ad8713c3536dc859b7d
shwetakhobare <shwetakhobare123@gmail.com>
2024-07-26 12:36:08 +0530

    python12 file is added

---
+++ python12.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number"))
+if num > 0:
+    print("It is a positive number")
+elif num == 0:
+    print("It is a zero number")
+else:
+    print("It is a negative number")
